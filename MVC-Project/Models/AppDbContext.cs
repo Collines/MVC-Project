f8a1bcd6820d3de0e69317ccf77a93ee53ab639a
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-20 07:22:07 +0200

    modified Model in _Layout.cshtml

--- MVC-Project/Models/AppDbContext.cs
+++ MVC-Project/Models/AppDbContext.cs
@@ -4,10 +4,18 @@ namespace shopping.Models
 {
     public class AppDBContext : DbContext
     {
+        public AppDBContext()
+        {
+           
+        }
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
 
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-VREBPAN\\MSSQLSERVER17;Initial Catalog=Shop-DB;Integrated Security=True;Pooling=False;Encrypt=True;TrustServerCertificate=True");
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Subcategory> Subcategories { get; set; }
         public DbSet<Product> Products { get; set; }
