e2f76d26f61fc750d9c6457c35fb58a253144f5b
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-24 19:19:05 +0200

    Added Wishlist entity, updated Product entity

---
+++ MVC-Project/Migrations/20230424171557_AddingWishListEntity.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingWishListEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wishlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Wishlists_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Wishlists_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlists_AccountID",
+                table: "Wishlists",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlists_ProductID",
+                table: "Wishlists",
+                column: "ProductID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wishlists");
+        }
+    }
+}
---
+++ MVC-Project/Models/Wishlist.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using MVC_Project.Models.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace shopping.Models
+{
+    public class Wishlist
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Account")]
+        [Required]
+
+        public int AccountID { get; set; }
+
+        [ForeignKey("Product")]
+        [Required]
+        public int ProductID { get; set; }
+        public virtual Account? Account { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
