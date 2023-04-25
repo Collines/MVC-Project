6f9e86c99d70ca233e33eff714b486dfb650cbee
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-25 11:43:48 +0200

    Added Vendor Authorization on Brand Controller

--- MVC-Project/Areas/Vendor/Controllers/BrandController.cs
+++ MVC-Project/Areas/Vendor/Controllers/BrandController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using shopping.Models;
 namespace MVC_Project.Areas.Vendor.Controllers
 {
     [Area("Vendor")]
+    [Authorize(Roles = "Vendor")]
     public class BrandController : Controller
     {
         private readonly AppDBContext _context;
