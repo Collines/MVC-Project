d2c11830a224c7c60bac6897e23322bb66d80206
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-25 11:11:07 +0200

    Added Vendor Dashboard Controller

---
+++ MVC-Project/Areas/Vendor/Controllers/VendorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Areas.Vendor.Controllers
+{
+    [Area("Vendor")]
+    [Authorize(Roles ="Vendor")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
