cc2dc4ec22ecf4964238448c543136580ab51a11
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-25 04:58:04 +0200

    update Product Brand Entity

---
+++ MVC-Project/Migrations/20230424221608_updateBrandEntity.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBrandEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Brand_brandId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+
+            migrationBuilder.RenameColumn(
+                name: "brandId",
+                table: "Products",
+                newName: "BrandID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_brandId",
+                table: "Products",
+                newName: "IX_Products_BrandID");
+
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Brands_BrandID",
+                table: "Products",
+                column: "BrandID",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Brands_BrandID",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.RenameColumn(
+                name: "BrandID",
+                table: "Products",
+                newName: "brandId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_BrandID",
+                table: "Products",
+                newName: "IX_Products_brandId");
+
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Brand_brandId",
+                table: "Products",
+                column: "brandId",
+                principalTable: "Brand",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
