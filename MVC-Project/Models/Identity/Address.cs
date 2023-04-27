c3a2801eb6a7d6c9c4d3481107468257004f8ed4
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-27 05:57:20 +0200

    Added Address Entity

---
+++ MVC-Project/Models/Identity/Address.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_Project.Models.Identity
+{
+    public class Address
+    {
+        int Id { get; set; }
+        [Required]
+        public required string Addr { get; set; }
+
+        [MaxLength(5)]
+        [RegularExpression("^(([0-9]*)|(([0-9]*)\\.([0-9]*)))$",ErrorMessage ="Appartment/Suite should be a number")]
+        public string? AppartmentSuite { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string TownCity { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string State { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression("^(([0-9]*)|(([0-9]*)\\.([0-9]*)))$", ErrorMessage = "Zipcode should be a number")]
+        public required string Zipcode { get; set; }
+
+        [ForeignKey("Account")]
+        [Required]
+        public int AccountId { get; set; }
+        public virtual Account? Account { get; set; }
+    }
+}
