8e3228f5879820430199869c0143532acd4b0fa5
yasmeenAhmed4 <58808451+yasmeenAhmed4@users.noreply.github.com>
2023-05-04 20:03:11 +0300

    seeding-Fashion Category

--- MVC-Project/Seeder/ImageSeeder.cs
+++ MVC-Project/Seeder/ImageSeeder.cs
@@ -8,78 +8,177 @@ namespace MVC_Project.Seeder
         {
             var images = new List<Image>
             {
+                // Watches
+                 new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B1.jpg"),
+                    ProductId = 1
+                },
+                 new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B2.jpg"),
+                    ProductId = 2
+                },
+                     new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B3.jpg"),
+                    ProductId = 3
+                },
+                  new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B4.jpg"),
+                    ProductId = 4
+                },
+                   new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B5.jpg"),
+                    ProductId = 5
+                },
+                    new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B6.jpg"),
+                    ProductId = 6
+                },
+                     new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B7.png"),
+                    ProductId = 7
+                },
+                      new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B8.jpg"),
+                    ProductId = 8
+                },
+                       new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/B9.jpg"),
+                    ProductId = 9
+                },
+
+
+                // Sunglasses
                 new Image
                 {
                     Title = "Image 1",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/1.jpg"),
-                    ProductId = 1
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S1.jpg"),
+                    ProductId = 10
                 },
+                 new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S2.jpg"),
+                    ProductId = 11
+                },
+                     new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S3.jpg"),
+                    ProductId = 12
+                },
+                  new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S4.png"),
+                    ProductId = 13
+                },
+                   new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S5.jpg"),
+                    ProductId = 14
+                },
+                    new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S6.jpg"),
+                    ProductId = 15
+                },
+                     new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S7.jpg"),
+                    ProductId = 16
+                },
+                      new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S8.png"),
+                    ProductId = 17
+                },
+                       new Image
+                {
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/S9.jpg"),
+                    ProductId = 18
+                },
+                 
+                       
+                // Luggage and Travel Gear
+
                 new Image
                 {
-                    Title = "Image 2",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/2.jpg"),
-                    ProductId = 1
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BD1.jpg"),
+                    ProductId = 19
                 },
-                new Image
+                 new Image
                 {
-                    Title = "Image 3",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/3.jpg"),
-                    ProductId = 2
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BD2.jpg"),
+                    ProductId = 20
                 },
-                new Image
+                     new Image
                 {
-                    Title = "Image 4",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/4.jpg"),
-                    ProductId = 2
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BD3.jpg"),
+                    ProductId = 21
                 },
-                new Image
+                  new Image
                 {
-                    Title = "Image 5",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/5.jpg"),
-                    ProductId = 3
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BA4.jpg"),
+                    ProductId = 22
                 },
-                new Image
+                   new Image
                 {
-                    Title = "Image 6",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/6.jpg"),
-                    ProductId = 3
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BA5.jpg"),
+                    ProductId = 23
                 },
-                new Image
+                    new Image
                 {
-                    Title = "Image 7",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/7.jpg"),
-                    ProductId = 4
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BA6.jpg"),
+                    ProductId = 24
                 },
-                new Image
+                     new Image
                 {
-                    Title = "Image 8",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/8.jpg"),
-                    ProductId = 4
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BS7.jpg"),
+                    ProductId = 25
                 },
-                new Image
+                      new Image
                 {
-                    Title = "Image 9",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/9.jpg"),
-                    ProductId = 5
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BS8.jpg"),
+                    ProductId = 26
                 },
-                new Image
+                       new Image
                 {
-                    Title = "Image 10",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/10.jpg"),
-                    ProductId = 5
+                    Title = "Image 1",
+                    ImageData = File.ReadAllBytes("wwwroot/products_images/BS9.jpg"),
+                    ProductId = 27
                 },
-                new Image
-                {
-                    Title = "Image 11",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/11.jpg"),
-                    ProductId = 6
-                },
-                new Image
-                {
-                    Title = "Image 12",
-                    ImageData = File.ReadAllBytes("wwwroot/products_images/12.jpg"),
-                    ProductId = 6
-                }
+
             };
 
             return images;
--- MVC-Project/wwwroot/js/min/all.js
+++ MVC-Project/wwwroot/js/min/all.js
@@ -739,69 +739,6 @@ Note: main.js, All Default Scripting Languages For This Theme Included In This F
 /*----------------------------------------------------------------------------------------------------*/
 /*------------------------------------------> The End <-----------------------------------------------*/
 /*----------------------------------------------------------------------------------------------------*/
-$(document).ready(function () {
-    function renderCartItems(cartItems) {
-        debugger;
-        var html = "";
-
-        cartItems.forEach(function (data) {
-            html +=
-                `
-                    <li id="newAdded${data.productId}" data-id="${data.productId}">
-                    <a href="/Product/Details/${data.productId}" class="minicart-product-image">
-                        <img src="${data.imageURI}" alt="cart products">
-                    </a>
-                    <div class="minicart-product-details">
-                        <h6><a href="/Product/Details/${data.productId}">${data.name}</a></h6>
-                        <span>${data.priceAfterDiscount} x ${data.quantity}</span>
-                    </div>
-                    <button class="close removeCartItem" data-id="${data.productId}">
-                        <i class="fa fa-close"></i>
-                    </button>
-                    </li>
-                `;
-        });
-
-        return html;
-    }
-
-    $(document).on("click", ".add-to-cart", function () {
-        console.log("Item added to cart");
-
-        var recoredToAdd = $(this).attr("data-id");
-        if (recoredToAdd != '') {
-            $.post("/ShoppingCart/AddToCart_v2/" + recoredToAdd,
-                function (data) {
-                    debugger;
-                    $('#cartItemCount').text(data.cartCount);
-                    $("#MiniCartTotalPrice").text(data.cartTotalPrice);
-                    $('#MiniCartTotalPrice2').text(data.cartTotalPrice);
-
-                    debugger;
-                    document.getElementsByClassName("minicart-product-list")[0].innerHTML = renderCartItems(data.cartItems);
-                });
-        }
-    });
-
-    $(document).on("click", ".removeCartItem", function () {
-        // Get the id from the link
-        var recordToDelete = $(this).attr("data-id");
-        var clicked = $(this);
-        if (recordToDelete != '') {
-            // Perform the ajax post
-            $.post("/ShoppingCart/RemoveFromCart/" + recordToDelete,
-                function (data) {
-                    debugger;
-                    $('#cartItemCount').text(data.cartCount);
-                    $("#MiniCartTotalPrice").text(data.cartTotalPrice);
-                    $('#MiniCartTotalPrice2').text(data.cartTotalPrice);
-
-                    debugger;
-                    document.getElementsByClassName("minicart-product-list")[0].innerHTML = renderCartItems(data.cartItems);
-                });
-        }
-    });
-});
 /*! jQuery Validation Plugin - v1.19.5 - 7/1/2022
  * https://jqueryvalidation.org/
  * Copyright (c) 2022 Jörn Zaefferer; Licensed MIT */
@@ -1241,3 +1178,66 @@ $(document).ready(function () {
 
     return $jQval.unobtrusive;
 }));
+
+    console.log('inside cart.js')
+    function renderCartItems(cartItems) {
+        debugger;
+        var html = "";
+
+        cartItems.forEach(function (data) {
+            html +=
+                `
+                    <li id="newAdded${data.productId}" data-id="${data.productId}">
+                    <a href="/Product/Details/${data.productId}" class="minicart-product-image">
+                        <img src="${data.imageURI}" alt="cart products">
+                    </a>
+                    <div class="minicart-product-details">
+                        <h6><a href="/Product/Details/${data.productId}">${data.name}</a></h6>
+                        <span>${data.priceAfterDiscount} x ${data.quantity}</span>
+                    </div>
+                    <button class="close removeCartItem" data-id="${data.productId}">
+                        <i class="fa fa-close"></i>
+                    </button>
+                    </li>
+                `;
+        });
+
+        return html;
+    }
+
+    $(document).on("click", ".add-to-cart", function () {
+        console.log("Item added to cart");
+
+        var recoredToAdd = $(this).attr("data-id");
+        if (recoredToAdd != '') {
+            $.post("/ShoppingCart/AddToCart_v2/" + recoredToAdd,
+                function (data) {
+                    debugger;
+                    $('#cartItemCount').text(data.cartCount);
+                    $("#MiniCartTotalPrice").text(data.cartTotalPrice);
+                    $('#MiniCartTotalPrice2').text(data.cartTotalPrice);
+
+                    debugger;
+                    document.getElementsByClassName("minicart-product-list")[0].innerHTML = renderCartItems(data.cartItems);
+                });
+        }
+    });
+
+    $(document).on("click", ".removeCartItem", function () {
+        // Get the id from the link
+        var recordToDelete = $(this).attr("data-id");
+        var clicked = $(this);
+        if (recordToDelete != '') {
+            // Perform the ajax post
+            $.post("/ShoppingCart/RemoveFromCart/" + recordToDelete,
+                function (data) {
+                    debugger;
+                    $('#cartItemCount').text(data.cartCount);
+                    $("#MiniCartTotalPrice").text(data.cartTotalPrice);
+                    $('#MiniCartTotalPrice2').text(data.cartTotalPrice);
+
+                    debugger;
+                    document.getElementsByClassName("minicart-product-list")[0].innerHTML = renderCartItems(data.cartItems);
+                });
+        }
+    });
